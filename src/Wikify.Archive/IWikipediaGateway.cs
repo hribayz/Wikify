5124417fc7ff90f4bded7e5128c27a540242240d
Tomáš Krupka <hribayz@gmail.com>
2021-03-20 21:09:25 +0000

    Add project files.

---
+++ src/Wikify.Archive/IWikipediaGateway.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Wikify.Archive
+{
+    public interface IWikipediaGateway
+    {
+        public string GetArticle(Uri articleUri);
+        public string GetArticle(string articleTitle);
+    }
+}
