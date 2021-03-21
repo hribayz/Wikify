7231d044a3d14398cc5b56f0c5739d0e1fcf7075
Tomáš Krupka <hribayz@gmail.com>
2021-03-21 20:12:52 +0000

    AWikiArticle design mock + todos.

--- src/Wikify.Common/AWikiArticle.cs
+++ src/Wikify.Common/AWikiArticle.cs
@@ -1,10 +1,32 @@
-﻿namespace Wikify.Common
+﻿using System.Threading.Tasks;
+
+namespace Wikify.Common
 {
+    // TODO: Parameterless constuctor for DI instantiation.
+    // TODO: Redo AWikiArticle asynchronously.
+
     /// <summary>
     /// Smart container for a wikipedia article. 
     /// </summary>
     public abstract class AWikiArticle
     {
-        public string ArticleHtml { get; private set; }
+        public string ArticleHtml { get; protected set; }
+        public AWikiArticle(string articleHtml)
+        {
+            LoadHtml(articleHtml);
+        }
+        public abstract void LoadHtml(string articleHtml);
+    }
+    public class WikiArticle : AWikiArticle
+    {
+        public WikiArticle(string articleHtml) : base(articleHtml)
+        {
+
+        }
+
+        public override void LoadHtml(string articleHtml)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
