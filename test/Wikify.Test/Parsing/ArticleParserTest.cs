c7ae49987a9dc31016575b214c01aa01003943ed
Tomáš Krupka <hribayz@gmail.com>
2021-05-13 12:57:52 +0100

    Tests

--- test/Wikify.Test/Parsing/ArticleParserTest.cs
+++ test/Wikify.Test/Parsing/ArticleParserTest.cs
@@ -23,20 +23,46 @@ namespace Wikify.Test.Parsing
         private static IArticleIdentifierFactory _articleIdentifierFactory;
         private static IArticleArchive _articleDownloader;
         private static IWikiArticleParser _articleParser;
+        private static INetworkingProvider _networkingProvider;
+        private static IArticleLicenseProvider _articleLicenseProvider;
+        private static ILicenseFactory _licenseFactory;
+        private static ICopyrightResolver _copyrightResolver;
+        private static ICopyrightFactory _copyrightFactory;
+        private static IWikiMediaFactory _wikiMediaFactory;
+        private static IAstTranslator _astTranslator;
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context)
         {
             _loggerFactory = new LoggerFactory();
             _articleIdentifierFactory = new ArticleIdentifierFactory();
-            _articleDownloader = new MediaWikiArticleDownloader(_loggerFactory.CreateLogger<MediaWikiArticleDownloader>(),
-                new NetworkingProvider(_loggerFactory.CreateLogger<NetworkingProvider>()),
-                new ArticleLicenseProvider(_loggerFactory.CreateLogger<ArticleLicenseProvider>(), 
-                    new LicenseFactory(),
-                    new CopyrightFactory(
-                        new CopyrightResolver())),
-                new WikiMediaFactory());
-            _articleParser = new ArticleParser(_loggerFactory.CreateLogger<ArticleParser>());
+            _licenseFactory = new LicenseFactory();
+            _copyrightResolver = new CopyrightResolver();
+            _wikiMediaFactory = new WikiMediaFactory();
+
+            _networkingProvider = new NetworkingProvider(
+                _loggerFactory.CreateLogger<NetworkingProvider>());
+
+            _copyrightFactory = new CopyrightFactory(
+                _copyrightResolver);
+
+            _articleLicenseProvider = new ArticleLicenseProvider(
+                _loggerFactory.CreateLogger<ArticleLicenseProvider>(),
+                _licenseFactory,
+                _copyrightFactory);
+
+            _articleDownloader = new MediaWikiArticleDownloader(
+                _loggerFactory.CreateLogger<MediaWikiArticleDownloader>(),
+                _networkingProvider,
+                _articleLicenseProvider,
+                _wikiMediaFactory);
+
+            _astTranslator = new PatternMatchingProvider(
+                _loggerFactory.CreateLogger<PatternMatchingProvider>());
+
+            _articleParser = new ArticleParser(
+                _loggerFactory.CreateLogger<ArticleParser>(),
+                _astTranslator);
         }
 
 
@@ -47,7 +73,7 @@ namespace Wikify.Test.Parsing
         {
             var article = await _articleDownloader.GetArticleAsync(_articleIdentifierFactory.GetIdentifier(title, Common.LanguageEnum.English), TextContentModel.WikiText);
 
-            await _articleParser.LoadArticleAsync(article);
+            await _articleParser.GetContainerAsync(article);
 
             ;
         }
