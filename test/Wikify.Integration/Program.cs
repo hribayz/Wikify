7c867d451d2698a1436a646ef260a93da73d08f7
Tomáš Krupka <hribayz@gmail.com>
2021-05-27 15:35:23 +0100

    Add Wikify.Integration project to directly run client code mockups.

---
+++ test/Wikify.Integration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Wikify.Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class ClientMock
+    {
+
+    }
+}
