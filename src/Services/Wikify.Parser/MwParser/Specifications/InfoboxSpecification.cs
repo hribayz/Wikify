1345d178ab7c5429cf4b67b4bbc7e2ba3c8fca6a
Tomáš Krupka <hribayz@gmail.com>
2021-06-07 11:22:32 +0100

    Implement Infobox specification.

---
+++ src/Services/Wikify.Parser/MwParser/Specifications/InfoboxSpecification.cs
@@ -0,0 +1,30 @@
+﻿using MwParserFromScratch.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Wikify.Common.Content;
+using Wikify.Common.Domain;
+
+namespace Wikify.Parser.MwParser.Specifications
+{
+    internal class InfoboxSpecification : PatternSpecification<Template>
+    {
+        private Func<Template, PatternMatch> _expression = 
+            template =>
+            {
+                if (template.GetTemplateName().ToLower().StartsWith("infobox"))
+                {
+                    return new PatternMatch(WikiComponentType.InfoPanel, template);
+                }
+                else
+                {
+                    return null;
+                }
+            };
+
+        internal override Func<Template, PatternMatch> Expression => _expression;
+    }
+}
---
+++ src/Services/Wikify.Parser/MwParser/Specifications/PatternSpecification.cs
@@ -0,0 +1,20 @@
+﻿using MwParserFromScratch.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wikify.Parser.MwParser.Specifications
+{
+    internal abstract class PatternSpecification<T> where T : Node
+    {
+        internal abstract Func<T, PatternMatch> Expression { get; }
+
+        internal PatternMatch GetMatchingPattern(T node)
+        {
+            return Expression(node);
+        }
+    }
+}
