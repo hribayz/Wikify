bcbc696be6e73957b530ad6bee6433de54f4bbaf
Tomáš Krupka <hribayz@gmail.com>
2021-06-24 10:49:26 +0100

    Update IWikiData interface to accomodate payload modification.

--- src/Services/Wikify.Common/Content/Parsed/IWikiComponent.cs
+++ src/Services/Wikify.Common/Content/Parsed/IWikiComponent.cs
@@ -9,9 +9,27 @@ namespace Wikify.Common.Content.Parsed
     /// </summary>
     public interface IWikiComponent
     {
+        /// <summary>
+        /// Original data of this component.
+        /// </summary>
         public IWikiData RawData { get; }
+        
+        /// <summary>
+        /// Gets the component type of this component.
+        /// </summary>
         public WikiComponentType ComponentType { get; }
+        
+        /// <summary>
+        /// Enumerates the children of this component.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<IWikiComponent> GetChildren();
+
+        /// <summary>
+        /// Enumerates the children of this component that pass given predicate.
+        /// </summary>
+        /// <param name="filter">Predicate to apply to each child.</param>
+        /// <returns>Children that have passed the predicate.</returns>
         public IEnumerable<IWikiComponent> GetChildren(Predicate<IWikiComponent> filter);
     }
 
--- src/Services/Wikify.Common/Content/Raw/IWikiData.cs
+++ src/Services/Wikify.Common/Content/Raw/IWikiData.cs
@@ -1,8 +1,19 @@
 ﻿namespace Wikify.Common.Content.Raw
 {
+    /// <summary>
+    /// An immutable container for storing the original content payload.
+    /// </summary>
     public interface IWikiData
     {
+        /// <summary>
+        /// Gets the payload in unmodified form.
+        /// </summary>
         public string Payload { get; }
+
+        /// <summary>
+        /// Gets the content model of the payload.
+        /// </summary>
         public ContentModel ContentModel { get; }
+        public IWikiData EncodeModifiedPayload(string modifiedPayload);
     }
 }
